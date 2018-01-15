6a8807f0ad9a1b56eb8113f27937e9299884a8cb
Hoyoung Lim <hoyounglim@192-168-1-97.tpgi.com.au>
2018-01-15 15:38:02 +1100

    JavaScript Update

---
+++ javascript/packageManager/index.js
@@ -0,0 +1,5 @@
+import moment from 'moment';
+
+console.log ("Hello, this is JavaScript");
+
+console.log(moment().startOf('year').fromNow());
---
+++ javascript/vuejs/quiz/script.js
@@ -0,0 +1,10 @@
+var question1 = new Vue ({
+  el:"#question1",
+  data: {
+    answers: ["China", "Mexico", "England", "Brazil"]
+  }
+})
+
+var question1 = function() {
+  
+}
